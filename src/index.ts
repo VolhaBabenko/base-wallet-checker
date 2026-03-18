d054687171a99c2b90286fb0caff611f3cc4e5c9
VolhaBabenko <154652563+VolhaBabenko@users.noreply.github.com>
2026-03-18 14:37:33 +0300

    feat: use format utility for input

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ console.log("Base Wallet Checker Lite")
 const address = process.argv[2]
 
 console.log("Wallet:", address)
+import { format } from './utils/format.js'
+
+const address = format(process.argv[2] || '')
