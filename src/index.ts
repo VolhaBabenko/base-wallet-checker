ea99864c76eba4685267b519379b497cbff92ece
VolhaBabenko <154652563+VolhaBabenko@users.noreply.github.com>
2026-03-18 14:49:01 +0300

    refactor: extract main logic into run function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-console.log("Starting app...")
-
 import { format } from './utils/format.js'
 
-const address = format(process.argv[2] || '')
+function run() {
+  console.log("Starting app...")
 
-if (!address) {
-  console.log("Please provide address")
-  process.exit(1)
+  const address = format(process.argv[2] || '')
+
+  if (!address) {
+    console.log("Please provide address")
+    process.exit(1)
+  }
+
+  const result = {
+    address
+  }
+
+  console.log(JSON.stringify(result, null, 2))
 }
 
-const result = {
-  address
-}
-
-console.log(JSON.stringify(result, null, 2))
+run()
